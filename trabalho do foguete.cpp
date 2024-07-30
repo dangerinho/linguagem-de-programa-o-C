3853a4a2887918c27a658940c63ad8fc43f01cd2
dangerinho <00001118733010sp@al.educacao.sp.gov.br>
2024-07-30 17:10:11 -0300

    Add files via upload

---
+++ trabalho do foguete.cpp
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main (){
+
+//variaveis//
+ printf ("o foguete esta pronto para a decolagem \n");
+ printf ("ele ira decolar apos o contador a seguir: \n");
+ printf ("contagem regressiva ate o 10 \n");
+int numero=1; 
+while (numero<=10){
+  printf("numero: %i \n",numero);
+	numero ++;
+	sleep(001);
+
+}																																			   		                                                                                                                                           
+ printf("                                                                    ##                                                               \n");        
+ printf("                                                                  ######                                                             \n") ;        
+ printf("                                                                  ##MM@@::                                                           \n")  ;       
+ printf("                                                                ####MMMM##                                                           \n")   ;      
+ printf("                                                                ##MMmmMMMM##                                                         \n")    ;     
+ printf("                                                              ++##MMMMMM@@##                                                         \n")     ;    
+ printf("                                                              ##mmMMmmMMMM@@##                                                       \n")      ;   
+ printf("                                                              ##MMMMMMMMMM@@##                                                       \n")       ;  
+ printf("                                                            ####mmMMMMMMMM@@##                                                       \n")        ; 
+ printf("                                                            ##mmMMMMMMMMMMMM@@##                                                     \n")         ;
+ printf("                                                            ##MMMMMMMMMMMMMM@@##                                                     \n")         ;
+ printf("                                                          ####MMMMmmmmmmMMMM@@@@MM                                                   \n")         ;
+ printf("                                                          ##@@++..            ..##                                                   \n")         ;
+ printf("                                                          ##                  ..##                                                   \n")         ;
+ printf("                                                        ####                  ....##                                                 \n")         ;
+ printf("                                                        ##                      ..##                                                 \n")         ;
+ printf("                                                        ##        --##MM##      ..##                                                 \n")         ;
+ printf("                                                        ##        ##MMmmmmMM    ..mmMM                                               \n")         ;
+ printf("                                                      ::##        MMmmmmmmmm    ....##                                               \n")         ;
+ printf("                                                      ##--        ##MMmmmm##    ....##                                               \n")         ;
+ printf("                                                      ##            ######        ..##                                               \n")         ;
+ printf("                                                      ##                          ..##++                                             \n")         ;
+ printf("                                                      ##                          ....##                                             \n")         ;
+ printf("                                                      ##                          ....##                                             \n")         ;
+ printf("                                                      ##            ::####        ....##                                             \n")         ;
+ printf("                                                      ##            ##MMmm##      ....##                                             \n")         ;
+ printf("                                                    ..##          MMMMmmmm##      ....##                                             \n")         ;
+ printf("                                                    --##            mmmmmm##      ....##                                             \n")         ;
+ printf("                                                    ::##              ####        ....##                                             \n")         ;
+ printf("                                                    ..##                          ....##                                             \n")         ;
+ printf("                                                      ##                          ....##                                             \n")         ;
+ printf("                                                      ##                          ....##                                             \n")         ;
+ printf("                                                      ##                          ....##                                             \n")         ;
+ printf("                                                      ##                          ....##                                             \n")         ;
+ printf("                                                      ##              ##MM##      ....##                                             \n")         ;
+ printf("                                                      ##@@          ##MMmmMM##    ....##                                             \n")         ;
+ printf("                                                        ##          MMmmmmmmMM..  ....##                                             \n")         ;
+ printf("                                                      ..##          MMmmmmmmMM    ..::####                                           \n")         ;
+ printf("                                                    ######          ##mmmmMM##    ..####@@MM##                                       \n")         ;
+ printf("                                                  ####MM@@##          --@@      ....##MM@@MMMMmm##                                   \n")         ;
+ printf("                                                ####MMMM@@##                    ....##@@@@MMMMMMMMMM##                               \n")         ;
+ printf("                                              ####MMMMMMMM##--                  ....##@@MMMMMMMMMMMMMM                               \n")         ;
+ printf("                                            ##@@MMMMMMMMMM@@##                  ..####@@@@MMMMMMMMMMMM                               \n")         ;
+ printf("                                            MMMMMMMMMMMM@@@@##                  ..####MM@@@@@@MMMMMMMM                               \n")         ;
+ printf("                                            ##MMMMMM@@@@@@######              ....##  --######MM@@MMMM                               \n")         ;
+ printf("                                            ##MM@@@@@@####    ##--            ....##          ########                               \n")         ;
+ printf("                                            ##@@@@####      ##########################                                               \n")         ;
+ printf("                                              ####          ##......................++                                               \n")         ;
+ printf("                                                              ..####################mm                                               \n")         ;
+ printf("                                                                ##            ....##                                                 \n")         ;
+ printf("                                                                ##            ....##                                                 \n")         ;
+ printf("                                                            ##########################                                               \n")         ;
+ printf("                                                            ##..........      ........                                               \n")         ;
+ printf("                                                                ######################                                               \n")         ;
+ printf("                                                                ##++++++##++++::++##                                                 \n")         ;
+ printf("                                                                ##++++++..##++++++++                                                 \n")         ;
+ printf("                                                                ++++++##    ##++++++##                                               \n")         ;
+ printf("                                                                ++++++##    ::++++++##                                               \n")         ;
+ printf("                                                              ##++++++..      ##++++##                                               \n")         ;
+ printf("                                                              ##++++++        ##++++++                                               \n")         ;
+ printf("                                                              ##++::##        ..::++++##                                             \n")         ;
+ printf("                                                              ##++++##          ++++++##                                             \n")         ;
+ printf("                                                              ##++::##::##      ##++++##                                             \n")         ;
+ printf("                                                              ##++++######  ##  ##++++##                                             \n")         ;
+ printf("                                                              @@++::##++++  ######++++##                                             \n")         ;
+ printf("                                                              mm++++::++++::##++MM++++@@                                             \n")         ;
+ printf("                                                              ++++++++++++##::++++++++mm                                             \n")         ;
+ printf("                                                              ++mm::++::++++++++++++++++                                             \n")         ;
+ printf("                                                              mm++::::++++++++++++::++++                                             \n")         ;
+ printf("                                                              ++++::++++++++++++++++++++--                                           \n")         ;
+ printf("                                                              ++++::++++++++++++++++++++mm                                           \n")         ;
+ printf("                                                              ++++++++++++++++++++++++++@@                                           \n")         ;
+ printf("                                                              mm++::++++++++++++++++++++@@                                           \n")         ;
+ printf("                                                              MM++++++++++++++++++++++++@@                                           \n")         ;
+ printf("                                                              @@++::++++++++++++++++++++@@                                           \n")         ;
+ printf("                                                              ##++++++++++++++++++++++++MM                                           \n")         ;
+ printf("                                                              ##++::++++++::++++++::++++MM                                           \n")         ;
+ printf("                                                              ##++##@@++++++++++++::++++::                                           \n")         ;
+ printf("                                                              @@##  ##++++++++++++++++##                                             \n")         ;
+ printf("                                                                    --++++++####++++++##                                             \n")         ;
+ printf("                                                                      ++++++..##++++####                                             \n")         ;
+ printf("                                                                      mm++##  ##++++##                                               \n")         ;
+ printf("                                                                      ##++##  ##++++##                                               \n")         ;
+ printf("                                                                      ####    ##++++mm                                               \n")         ;
+ printf("                                                                              ##++++                                                 \n")         ;
+ printf("                                                                               ++##                                                  \n")        ;
+ printf("                                                                               ++##                                                  \n")        ;
+ printf("                                                                               ####                                                  \n")        ;
+ printf("                                                                               @@                                                    \n")        ;
+	
+printf("o aviao decolou com sucesso total, deixe em tela cheia para observar os processos \n");
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
